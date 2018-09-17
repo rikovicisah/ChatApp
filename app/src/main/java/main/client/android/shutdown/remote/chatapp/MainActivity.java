3f78c2943721418c88fd7da3e6c5fc1d27332dd6
rikovicisah <rikovicisah@gmail.com>
2018-09-18 00:53:47 +0200

    Osnovne izmijene

---
+++ app/src/main/java/main/client/android/shutdown/remote/chatapp/MainActivity.java
@@ -0,0 +1,13 @@
+package main.client.android.shutdown.remote.chatapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
