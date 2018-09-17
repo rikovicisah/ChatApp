764ca4be05f3b1f17ea72ebe31ddf427848eff7e
rikovicisah <rikovicisah@gmail.com>
2018-09-18 01:12:05 +0200

    Provjera da li je korisnik logiran

--- app/src/main/java/main/client/android/shutdown/remote/chatapp/MainActivity.java
+++ app/src/main/java/main/client/android/shutdown/remote/chatapp/MainActivity.java
@@ -1,13 +1,39 @@
 package main.client.android.shutdown.remote.chatapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+
+//Activity koji ce prikazvati listu prijatelja
+//koji ce provjeriti da li smo logirani
+//u slucaju da nismo pozvat ce Home activity
 public class MainActivity extends AppCompatActivity {
+
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mAuth = FirebaseAuth.getInstance();
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+
+        //u slucaju da nije logiran preusmjeriti na home acitivity
+        if(currentUser == null){
+            Intent homeActivity = new Intent(MainActivity.this, HomeActivity.class);
+            startActivity(homeActivity);
+            finish();
+        }
     }
 }
