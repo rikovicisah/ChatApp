5f37ed0140ea048936273edabfbf5e028152d3f4
rikovicisah <rikovicisah@gmail.com>
2018-09-27 22:41:44 +0200

    Ispis username-a, statusa //TODO sliku upload

--- app/src/main/java/main/client/android/shutdown/remote/chatapp/AccountSettingsActivity.java
+++ app/src/main/java/main/client/android/shutdown/remote/chatapp/AccountSettingsActivity.java
@@ -5,15 +5,32 @@ import android.os.Bundle;
 import android.widget.Button;
 import android.widget.TextView;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
 import de.hdodenhof.circleimageview.CircleImageView;
 
 public class AccountSettingsActivity extends AppCompatActivity {
 
     Button accountSettings_btnChangeStatus;
     Button accountSettings_btnChangeImage;
+
     TextView accountSettings_txtName;
     TextView accountSettings_txtStatus;
     CircleImageView profile_image;
+
+    DatabaseReference databaseReference;
+    FirebaseUser firebaseUser;
+
+    String username;
+    String status;
+    String imageD;
+    String thumbimage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,8 +39,37 @@ public class AccountSettingsActivity extends AppCompatActivity {
 
         accountSettings_btnChangeStatus = findViewById(R.id.accountSettings_btnChangeStatus);
         accountSettings_btnChangeImage = findViewById(R.id.accountSettings_btnChangeImage);
+
         accountSettings_txtName = findViewById(R.id.accountSettings_txtName);
         accountSettings_txtStatus = findViewById(R.id.accountSettings_txtStatus);
         profile_image = findViewById(R.id.profile_image);
+
+        firebaseUser = FirebaseAuth.getInstance().getCurrentUser();
+        if(firebaseUser != null) {
+            databaseReference = FirebaseDatabase.getInstance()
+                    .getReference()
+                    .child("Users")
+                    .child(firebaseUser.getUid().toString());
+
+            databaseReference.addValueEventListener(new ValueEventListener() {
+                @Override
+                public void onDataChange(DataSnapshot dataSnapshot) {
+                    username = dataSnapshot.child("username").getValue().toString();
+                    status = dataSnapshot.child("status").getValue().toString();
+                    imageD = dataSnapshot.child("image").getValue().toString();
+                    thumbimage = dataSnapshot.child("thumbImage").getValue().toString();
+
+                    accountSettings_txtName.setText(username);
+                    accountSettings_txtStatus.setText(status);
+                }
+
+                @Override
+                public void onCancelled(DatabaseError databaseError) {
+
+                }
+            });
+        }
+
+
     }
 }
